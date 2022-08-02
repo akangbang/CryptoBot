9e860fec2fdaa1477d69ef03f5d71836b5b902be
akangbang <93835548+akangbang@users.noreply.github.com>
2022-08-02 12:57:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import discord
 
+# instantiate a discord client
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print(f'You have logged in as {client}')
+  
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('Hi'):
+    await message.channel.send('Hello')
+  
+  def getCryptoPrices(crypto):
+    URL ='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
+    r = requests.get(url=URL)
+    data = r.json()
+
+
+
+BOT_TOKEN = 'MTAwMjA1NDY4MTc1OTA2MDAyOA.GGbtJs.VdrSY2av8FVSx9JqoTHtBMXWSJwUpXNKUrbO44'
+client.run(BOT_TOKEN)
